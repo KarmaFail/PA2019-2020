a95ea594bca0658fcdbb053130db1c4fc44e0390
KarmaFail <50875605+KarmaFail@users.noreply.github.com>
2020-07-08 13:43:45 +0200

    Update TestLVM.tf

--- terraform/TestLVM.tf
+++ terraform/TestLVM.tf
@@ -5,27 +5,6 @@ provider "proxmox" {
     pm_password = "Espoir15"
 }
 
-resource "proxmox_lxc" "lxc-test" { 
-    hostname = "lxc-test-host"
-    cores = 1
-    memory = "1024"
-    swap = "2048"
-    network {
-        name = "eth0"
-        bridge = "vmbr2"
-        ip = "192.168.20.15/24"
-        gw = "192.168.20.254" 
-    }
-    ostemplate = "local:vztmpl/debian-10.0-standard_10.0-1_amd64.tar.gz" 
-    password = "Espoir15"
-    vmid = "201"
-    storage = "local"
-    rootfs = "local:8" 
-    target_node = "PAR-212482"
-    unprivileged = false
-    onboot = true
-}
-
 resource "proxmox_lxc" "lxc-test2" { 
     hostname = "lxc-test-host-2"
     cores = 1

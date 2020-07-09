f92ef84b4e73b3f5db4790ddba7ef536c8b09964
KarmaFail <50875605+KarmaFail@users.noreply.github.com>
2020-07-09 15:30:44 +0200

    Update minio.tf

--- terraform/Minio/minio.tf
+++ terraform/Minio/minio.tf
@@ -1,18 +1,26 @@
 resource "proxmox_lxc" "Minio-1" { 
     hostname = "Minio-1"
-    cores = 2
-    memory = "2048"
-    swap = "2048"
+    cores = 4
+    memory = "4096"
+    swap = "4096"
     network {
         name = "eth0"
         bridge = "vmbr5"
         ip = "192.168.30.61/24"
         gw = "192.168.30.254"
     }
+    network {
+        name = "eth1"
+        bridge = "vmbr5"
+    }
+    mountpoint {
+        mp = "/mnt/data1"
+        size = "80G"
+    }
     ostemplate = "local:vztmpl/debian-10.0-standard_10.0-1_amd64.tar.gz" 
     password = "Espoir15"
     vmid = "361"
-    rootfs = "local:361" 
+    rootfs = "local:8" 
     storage = "local"
     target_node = "PAR-212482"
     unprivileged = false
@@ -20,19 +28,27 @@ resource "proxmox_lxc" "Minio-1" {
 
 resource "proxmox_lxc" "Minio-2" { 
     hostname = "Minio-2"
-    cores = 2
-    memory = "2048"
-    swap = "2048"
+    cores = 4
+    memory = "4096"
+    swap = "4096"
     network {
         name = "eth0"
         bridge = "vmbr5"
         ip = "192.168.30.62/24"
         gw = "192.168.30.254"  
     }
+    network {
+        name = "eth1"
+        bridge = "vmbr5"
+    }
+    mountpoint {
+        mp = "/mnt/data1"
+        size = "80G"
+    }
     ostemplate = "local:vztmpl/debian-10.0-standard_10.0-1_amd64.tar.gz" 
     password = "Espoir15"
     vmid = "362"
-    rootfs = "local:362" 
+    rootfs = "local:8" 
     storage = "local"
     target_node = "PAR-212482"
     unprivileged = false

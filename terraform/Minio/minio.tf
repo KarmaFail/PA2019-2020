d1c1a0e2e97f44ab8bcf75d4a84c63134fc8cddb
KarmaFail <50875605+KarmaFail@users.noreply.github.com>
2020-07-09 15:33:30 +0200

    Update minio.tf

--- terraform/Minio/minio.tf
+++ terraform/Minio/minio.tf
@@ -14,7 +14,7 @@ resource "proxmox_lxc" "Minio-1" {
         bridge = "vmbr5"
     }
     mountpoint {
-        volume = data1.raw
+        volume = "data1.raw"
         mp = "/mnt/data1"
         size = 80
     }
@@ -43,7 +43,7 @@ resource "proxmox_lxc" "Minio-2" {
         bridge = "vmbr5"
     }
     mountpoint {
-        volume = data2.raw
+        volume = "data2.raw"
         mp = "/mnt/data2"
         size = 80
     }
